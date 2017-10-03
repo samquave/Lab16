052ca47c682276e056ca87959d5e75a45b90c308
sam quave <samquave@sams-MacBook-Air.local>
2017-10-03 18:23:38 -0400

    functioning chirper with added functionality

--- client/script.js
+++ client/script.js
@@ -1,105 +1,92 @@
-var $submitBtn = $('#chirp-submit');
-var $chirpInput = $('#chirp-input-box');
+var $chirpButton = $('#chirp-submit');
+var $chirpField = $('#chirp-input-box');
 var $chirpList = $('#chirp-field');
-var $userDropdown = $('#dropdown');
+var $userSelector = $('#dropdown');
 
-$submitBtn.click(postChirp);
-
-$chirpInput.on('input', function () {
-    var isEmpty = $chirpInput.val().length === 0;
-    $submitBtn.prop('disabled', isEmpty);
+$chirpField.on('input', function() {
+    var isEmpty = $chirpField.val().length === 0;
+    $chirpButton.prop('disabled', isEmpty);
 });
+$chirpButton.click(postChirp);
 
 function postChirp() {
     var chirp = {
-        message: $chirpInput.val(),
-        user: $userDropdown.val(),
-
+        message: $chirpField.val(),
+        userid: $userSelector.val()
     };
     $.ajax({
         method: 'POST',
         url: '/api/chirps',
         contentType: 'application/json',
         data: JSON.stringify(chirp)
-    }).then(function (success) {
-        $chirpInput.val('');
-        $submitBtn.prop('disabled', true);
+    }).then(function(success) {
+        $chirpField.val('');
+        $chirpButton.prop('disabled', true);
         getChirps();
-    }, function (err) {
-        console.log(err);
-    })
-
-
-}
-function getChirps() {
-    $.ajax({
-        method: 'GET',
-        url: '/api/chirps'
-
-    }).then(function (chirps) {
-        $chirpList.empty();
-        for (var i = 0; i < chirps.length; i++) {
-            var $chirpDiv = $('<div class="chirp-div"></div>');
-            var $message = $('<p></p>');
-            var $user = $('<h4></h4>');
-            var $timestamp = $('<h5></h5>');
-            var $deleteBtn = $('<button id="delete-btn"><span class= "glyphicon glyphicon-trash"></span></button>');
-            // $deleteBtn.click(deleteChirp(chirps[i].id));
-
-            $message.text(chirps[i].message);
-            $user.text(chirps[i].user);
-            $timestamp.text(new Date(chirps[i].timestamp).toLocaleString());
-            $message.appendTo($chirpDiv);
-            $user.appendTo($chirpDiv);
-            $timestamp.appendTo($chirpDiv);
-            $deleteBtn.appendTo($chirpDiv);
-            $chirpDiv.appendTo($chirpList);
-
-        }
-
-    }, function (err) {
+    }, function(err) {
         console.log(err);
     });
 }
 
-function getUsers() {
+function getChirps() {
+    $.ajax({
+        method: 'GET',
+        url: '/api/chirps',
+    }).then(function(chirps) {
+        $chirpList.empty();
+        for (var i = 0; i < chirps.length; i++) {
+            addChirpDiv(chirps[i]);
+        }
+    }, function(err) {
+        console.log(err);
+    });
+}
+getChirps();
+
+function addChirpDiv(chirp) {
+    var $chirpDiv = $('<div class="chirp-div"></div>');
+    var $message = $('<p></p>');
+    var $user = $('<h4></h4>');
+    var $timestamp = $('<h5></h5>');
+    var $delButton = $('<button class="delete-button"><span class="glyphicon glyphicon-trash"></span></button>');
+    $delButton.click(function() {
+        deleteChirp(chirp.id);
+    });
+
+    $message.text(chirp.message);
+    $user.text(chirp.userName);
+    $timestamp.text(new Date(chirp.timestamp).toLocaleString());
+
+    $message.appendTo($chirpDiv);
+    $user.appendTo($chirpDiv);
+    $timestamp.appendTo($chirpDiv);
+    $delButton.appendTo($chirpDiv);
+
+    $chirpDiv.appendTo($chirpList);
+}
+
+function deleteChirp(id) {
+    $.ajax({
+        method: 'DELETE',
+        url: '/api/chirps/' + id
+    }).then(function() {
+        getChirps();
+    }, function(err) {
+        console.log(err);
+    });
+}
+
+function populateUsers() {
     $.ajax({
         method: 'GET',
         url: '/api/users'
-    }).then(function (u) {
-        for (var i = 0; i < u.length; i++) {
-
-            var $userOption = $('<option value="' + u[i].Users + '">' + u[i].Users + '</option>');
-            
-            $userOption.text(u[i].Users);
-            $userOption.appendTo($userDropdown);
-
+    }).then(function(users) {
+        for (var i = 0; i < users.length; i++) {
+            var $userOption = $('<option value="' + users[i].id + '">' + users[i].name + '</option>');
+            $userSelector.append($userOption);
         }
-    }, function (err) {
+    }, function(err) {
         console.log(err);
-    })
+    });
 }
-
-getUsers();
-
-
-// function deleteChirp(id) {
-
-//     $.ajax({
-//         method: 'DELETE',
-//         url: '/api/chirps/' + id
-
-
-
-//     }).then(function () {
-//         getChirps();
-//         // $chirpDiv.remove();
-//     }), function (err) {
-//         console.log(err);
-//     }
-// }
-
-
-
-
-getChirps();
+populateUsers();
--- server/index.js
+++ server/index.js
@@ -1,151 +1,138 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-var fs = require('fs');
-var mysql = require('mysql');
+var express = require("express");
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
+var path = require("path");
+var app = express();
 
 var pool = mysql.createPool({
-    connectionLimit: 10,
-    host: 'localhost',
-    user: 'chirperuser',
-    password: 'chirppass',
-    database: 'Chirper'
+  connectionLimit: 10,
+  host: "localhost",
+  user: "chirperuser",
+  password: "chirppass",
+  database: "Chirper"
 });
 
+var clientPath = path.join(__dirname, "../client");
+// var dataPath = path.join(__dirname, 'data.json');
 
-
-
-var clientPath = path.join(__dirname, '../client');
-var dataPath = (path.join(__dirname, 'data.json'));
-var app = express();
 app.use(express.static(clientPath));
-
 app.use(bodyParser.json());
 
+app
+  .route("/api/chirps")
+  .get(function(req, res) {
+    rows("GetAllChirps")
+      .then(function(chirps) {
+        res.send(chirps);
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.sendStatus(500);
+      });
+  })
+  .post(function(req, res) {
+    var newChirp = req.body;
+    row("InsertChirp", [newChirp.message, newChirp.userid])
+      .then(function(id) {
+        res.status(201).send(id);
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.sendStatus(500);
+      });
+  });
 
+app
+  .route("/api/chirps/:id")
+  .get(function(req, res) {
+    row("GetSingleChirp", [req.params.id])
+      .then(function(chirp) {
+        res.send(chirp);
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.sendStatus(500);
+      });
+  })
+  .put(function(req, res) {
+    empty("UpdateChirp", [req.params.id, req.body.message])
+      .then(function() {
+        res.sendStatus(204);
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.sendStatus(500);
+      });
+  })
+  .delete(function(req, res) {
+    empty("DeleteChirp", [req.params.id])
+      .then(function() {
+        res.sendStatus(204);
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.sendStatus(500);
+      });
+  });
 
-app.get('/api/users', function(req, res){
-    getUsers()
-        .then(function (users) {
-            res.send(users);
-        }), function (err) {
-            res.status(500).send(err);
-        }
-})
-
-
-app.route('/api/chirps')
-    .get(function (req, res) {
-        getChirps()
-            .then(function (chirps) {
-                res.send(chirps);
-            }), function (err) {
-                res.status(500).send(err);
-            }
-
-
-
-    }).post(function (req, res) {
-        writeChirp(req.body.message, req.body.user, req.body.timestamp)
-            .then(function () {
-                res.send();
-            }), function (err) {
-                res.status(500).send(err);
-            }
-
+app.route("/api/users").get(function(req, res) {
+  rows("GetUsers")
+    .then(function(users) {
+      res.send(users);
     })
-    
-// app.delete('/api/chirps/:id', function (req, res) {
-//         deleteChirp(req.body.id)
-//             .then(function () {
-//                 res.send();
-//             }), function (err) {
-//                 res.status(500).send(err);
-//             }
-//     })
-
-
+    .catch(function(err) {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 app.listen(3000);
 
-
-
-function getChirps() {
-    return new Promise(function (fulfill, reject) {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                reject(err);
+function callProcedure(procedureName, args) {
+  return new Promise(function(resolve, reject) {
+    pool.getConnection(function(err, connection) {
+      if (err) {
+        reject(err);
+      } else {
+        var placeholders = "";
+        if (args && args.length > 0) {
+          for (var i = 0; i < args.length; i++) {
+            if (i === args.length - 1) {
+              // if we are on the last argument in the array
+              placeholders += "?";
             } else {
-                connection.query("CALL GetAllChirps();", function (err, resultsets) {
-                    if (err) {
-                        connection.release();
-                        reject(err);
-                    } else {
-                        connection.release();
-                        fulfill(resultsets[0]);
-                    }
-                });
+              placeholders += "?,";
             }
+          }
+        }
+        var callString = "CALL " + procedureName + "(" + placeholders + ");"; // CALL GetChirps();, or CALL InsertChirp(?,?,?);
+        connection.query(callString, args, function(err, resultsets) {
+          connection.release();
+          if (err) {
+            reject(err);
+          } else {
+            resolve(resultsets);
+          }
         });
+      }
     });
+  });
 }
 
-function writeChirp(messageVal, userVal, timestampVal) {
-    return new Promise(function (fulfill, reject) {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                reject(err);
-            } else {
-                connection.query("CALL InsertChirp(?, ?, ?);", [messageVal, userVal, timestampVal], function (err, resultsets) {
-                    if (err) {
-                        connection.release();
-                        reject(err);
-                    } else {
-                        connection.release();
-                        fulfill(resultsets[0]);
-                    }
-                });
-            }
-        });
-    });
+function rows(procedureName, args) {
+  return callProcedure(procedureName, args).then(function(resultsets) {
+    return resultsets[0];
+  });
 }
 
-function deleteChirp(idVal) {
-    return new Promise(function (fulfill, reject) {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                reject(err);
-            } else {
-                connection.query("CALL DeleteChirp(?);", [idVal], function (err, resultsets) {
-                    if (err) {
-                        connection.release();
-                        reject(err);
-                    } else {
-                        connection.release();
-                        fulfill(resultsets[0]);
-                    }
-                });
-            }
-        });
-    });
+function row(procedureName, args) {
+  return callProcedure(procedureName, args).then(function(resultsets) {
+    return resultsets[0][0];
+  });
 }
 
-function getUsers() {
-    return new Promise(function (fulfill, reject) {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                reject(err);
-            } else {
-                connection.query("CALL GetUsers();", function (err, resultsets) {
-                    if (err) {
-                        connection.release();
-                        reject(err);
-                    } else {
-                        connection.release();
-                        fulfill(resultsets[0]);
-                    }
-                });
-            }
-        });
-    });
+function empty(procedureName, args) {
+  return callProcedure(procedureName, args).then(function() {
+    return;
+  });
 }
